b39151e065de7b964b239d64dd77489fc890186c
skullKim <kyk990328@gmail.com>
2021-09-20 20:57:13 +0900

    추가: db 연동 완료

--- dist/src/app.js
+++ dist/src/app.js
@@ -4,19 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var models_1 = require("./models");
+var models_1 = require("../models");
 var app = (0, express_1.default)();
 app.set('port', process.env.PORT || 8080);
-models_1.sequelize
-    .sync({ force: false })
-    .then(function () {
-    // eslint-disable-next-line no-console
-    console.log('success to connect DB');
-})
-    .catch(function (err) {
-    // eslint-disable-next-line no-console
-    console.error(err);
-});
+models_1.sequelize.sync({ force: false })
+    .then(function () { return console.log('success to connect DB'); })
+    .catch(function (err) { return console.error(err); });
 app.get('/', function (req, res) {
     res.send('hi');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import express from 'express';
 
+import { sequelize } from './models';
+
 const app: express.Application = express();
 
 app.set('port', process.env.PORT || 8080);
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('success to connect DB');
+  })
+  .catch((err: String) => {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  });
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('hi');
