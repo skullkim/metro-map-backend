e7e80bbff94c402548bc8f2d5d422c5100ca4d4b
skullKim <kyk990328@gmail.com>
2021-09-23 19:18:26 +0900

    추가: typeorm 연동 완료

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,20 @@
 import express from 'express';
+import {createConnection} from 'typeorm';
 
-import { sequelize } from './models';
+/* eslint-disable */
+createConnection().then(connection => {
+  const app: express.Application = express();
 
-const app: express.Application = express();
+  app.set('port', process.env.PORT || 8080);
 
-app.set('port', process.env.PORT || 8080);
-
-sequelize
-  .sync({ force: false })
-  .then(() => {
-    // eslint-disable-next-line no-console
-    console.log('success to connect DB');
-  })
-  .catch((err: String) => {
-    // eslint-disable-next-line no-console
-    console.error(err);
+  app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('hi');
   });
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('hi');
+  app.listen(app.get('port'), () => {
+    // eslint-disable-next-line no-console
+    console.log(`${app.get('port')} server start`);
+  });
+
 });
 
-app.listen(app.get('port'), () => {
-  // eslint-disable-next-line no-console
-  console.log(`${app.get('port')} server start`);
-});
