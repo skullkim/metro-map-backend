930e77e17518bc32cd2c5ff7da8faa704a552866
skullKim <kyk990328@gmail.com>
2021-09-27 16:55:10 +0900

    추가: 서버 개발 시작 전 필요한 에러처리, 기본적인 모듈 적용 완료

--- dist/src/app.js
+++ dist/src/app.js
@@ -5,12 +5,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var typeorm_1 = require("typeorm");
+var cookie_parser_1 = __importDefault(require("cookie-parser"));
+var dotenv_1 = __importDefault(require("dotenv"));
+var morgan_1 = __importDefault(require("morgan"));
 /* eslint-disable */
 (0, typeorm_1.createConnection)().then(function (connection) {
     var app = (0, express_1.default)();
+    dotenv_1.default.config();
     app.set('port', process.env.PORT || 8080);
-    app.get('/', function (req, res) {
-        res.send('hi');
+    app.use((0, morgan_1.default)('dev'));
+    app.use(express_1.default.json());
+    app.use(express_1.default.urlencoded({ extended: true }));
+    app.use((0, cookie_parser_1.default)());
+    app.use(function (req, response, next) {
+        var error = new Error(req.method + " " + req.originalUrl + " router doesn't exist");
+        error.status = 400;
+        next(error);
+    });
+    app.use(function (err, req, res, next) {
+        res.locals.message = err.message;
+        res.locals.error = process.env.NODE_DEV !== 'production' ? err : {};
+        res.send(res.locals.message);
     });
     app.listen(app.get('port'), function () {
         // eslint-disable-next-line no-console
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,38 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { createConnection } from 'typeorm';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+import {ReqError, HttpException} from './typeLib/Error';
 
 /* eslint-disable */
 createConnection().then(connection => {
   const app: express.Application = express();
 
+  dotenv.config();
   app.set('port', process.env.PORT || 8080);
 
-  app.get('/', (req: express.Request, res: express.Response) => {
+  app.use(morgan('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({extended: true}));
+  app.use(cookieParser());
+
+  app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('hi');
-  });
+  })
+
+  app.use((req: Request, response: Response, next: NextFunction) => {
+    const error: ReqError = new Error(`${req.method} ${req.originalUrl} router doesn't exist`);
+    error.status = 400;
+    next(error);
+  })
+
+  app.use((err: HttpException, req: Request, res: Response, next: NextFunction) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_DEV !== 'production' ? err : {};
+    res.send(res.locals.message);
+  })
 
   app.listen(app.get('port'), () => {
     // eslint-disable-next-line no-console
