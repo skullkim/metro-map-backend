72206e38c412e609dc9587e84f5f70db2b28a92f
skullKim <kyk990328@gmail.com>
2021-11-02 16:32:45 +0900

    추가: 검색 기록 데이터를 사용하는데 필요한 메서드 currentSearched에 추가

--- src/models/currentSearched.ts
+++ src/models/currentSearched.ts
@@ -1,9 +1,9 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Column, BaseEntity } from 'typeorm';
 
 import { User } from './user';
 
 @Entity()
-export class CurrentSearched {
+export class CurrentSearched extends BaseEntity {
   @PrimaryGeneratedColumn()
   id!: number;
 
@@ -24,9 +24,64 @@ export class CurrentSearched {
   })
   stopover!: string;
 
-  @Column()
+  @Column({
+    default: false,
+  })
   bookmark!: boolean;
 
   @ManyToOne(() => User, (user) => user.targetUser)
   user!: User;
+
+  static async setSearchHistory(from: string, to: string, stopover: string, user: User) {
+    try {
+      const prevHistory = await CurrentSearched.getUserSearchHistory(user.id);
+      if(prevHistory.length > 6) {
+        await CurrentSearched.deleteSearchHistory(prevHistory[0].id);
+      }
+      return this.createQueryBuilder('currentSearched')
+        .insert()
+        .into(CurrentSearched)
+        .values({from, to, stopover, user})
+        .execute();
+    }
+    catch(err) {
+      return err;
+    }
+  }
+
+  static getBookMark(id: number) {
+    return this.createQueryBuilder('currentSearched')
+      .where('currentSearched.id = :id', {id})
+      .getOne();
+  }
+
+  static async checkBookmark(id: number) {
+    try {
+      const prevBookmark: boolean | undefined = (await CurrentSearched.getBookMark(id))?.bookmark;
+      return this.createQueryBuilder('currentSearched')
+        .update(CurrentSearched)
+        .set({bookmark: !prevBookmark})
+        .where('id = :id', {id})
+        .execute();
+    }
+    catch(err) {
+      return err;
+    }
+  }
+
+  static getUserSearchHistory(userId: number) {
+    return this.createQueryBuilder('currentSearched')
+      .innerJoin('currentSearched.user', 'user')
+      .where('user.id = :userId', {userId})
+      .orderBy('currentSearched.id', 'ASC')
+      .getMany();
+  }
+
+  static deleteSearchHistory(id: number) {
+    return this.createQueryBuilder('currentSearched')
+      .delete()
+      .from(CurrentSearched)
+      .where('id = :id', {id})
+      .execute();
+  }
 }
